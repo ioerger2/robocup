527a23f17a428e5795d085fe48da36d73ba7746e
Tom Ioerger <ioerger@tamu.edu>
2018-02-13 08:02:42 -0600

    split Client class inferface out into header file

--- rcssserver-15.3.0/src/client.cpp
+++ rcssserver-15.3.0/src/client.cpp
@@ -1,70 +1,8 @@
-// -*-c++-*-
+#include "client.hpp"
 
-/***************************************************************************
-                          client.cpp  -  A basic client that connects to
-                          the server
-                             -------------------
-    begin                : 27-DEC-2001
-    copyright            : (C) 2001 by The RoboCup Soccer Server
-                           Maintenance Group.
-    email                : sserver-admin@lists.sourceforge.net
-***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU LGPL as published by the Free Software  *
- *   Foundation; either version 3 of the License, or (at your option) any  *
- *   later version.                                                        *
- *                                                                         *
- ***************************************************************************/
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-
-#include "compress.h"
-
-#include <rcssbase/net/socketstreambuf.hpp>
-#include <rcssbase/net/udpsocket.hpp>
-#include <rcssbase/gzip/gzstream.hpp>
-
-#ifdef HAVE_SSTREAM
-#include <sstream>
-#else
-#include <strstream>
-#endif
-#include <iostream>
-#include <cerrno>
-#include <csignal>
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-
-#ifdef __CYGWIN__
-// cygwin is not win32
-#elif defined(_WIN32) || defined(__WIN32__) || defined (WIN32)
-#  define RCSS_WIN
-#  include <winsock2.h>
-#endif
-
-#ifdef HAVE_UNISTD_H
-#include <unistd.h> // select()
-#endif
-#ifdef HAVE_SYS_SELECT_H
-#include <sys/select.h> // select()
-#endif
-#ifdef HAVE_SYS_TIME_H
-#include <sys/time.h> // select()
-#endif
-#ifdef HAVE_SYS_TYPES_H
-#include <sys/types.h> // select()
-#endif
-
-//------------------------------------------------------------------------------
+// TRI
 
 #include <math.h>
-#include "Sexpr.hpp"
 
 #define PI 3.1415927
 
@@ -74,27 +12,7 @@ float dist(float X1,float Y1,float X2,float Y2) { return sqrt(pow(X1-X2,2)+pow(Y
 
 //------------------------------------------------------------------------------
 
-class Client {
-//private:
-public:
-    rcss::net::Addr M_dest;
-    rcss::net::UDPSocket M_socket;
-    rcss::net::SocketStreamBuf * M_socket_buf;
-    rcss::gz::gzstreambuf * M_gz_buf;
-    std::ostream * M_transport;
-    int M_comp_level;
-    bool M_clean_cycle;
-
-#ifdef HAVE_LIBZ
-    Decompressor M_decomp;
-#endif
-
-    Client();
-    Client( const Client & );
-    Client & operator=( const Client & );
-
-    Client( const std::string & server,
-            const int port )
+Client::Client( const std::string & server, const int port )
         : M_dest( port ),
           M_socket(),
           M_socket_buf( NULL ),
@@ -110,20 +28,17 @@ public:
           M_socket_buf->setEndPoint( M_dest );
       }
 
-    virtual
-    ~Client()
+Client::~Client()
       {
           close();
       }
 
-    void run()
+void Client::run()
       {
           messageLoop();
       }
 
-//private:
-
-    int open()
+int Client::open()
       {
           if ( M_socket.open() )
           {
@@ -150,7 +65,7 @@ public:
           return 0;
       }
 
-    bool bind()
+bool Client::bind()
       {
           if ( ! M_socket.bind( rcss::net::Addr() ) )
           {
@@ -162,7 +77,7 @@ public:
           return true;
       }
 
-    void close()
+void Client::close()
       {
           M_socket.close();
 
@@ -185,7 +100,7 @@ public:
           }
       }
 
-    int setCompression( int level )
+int Client::setCompression( int level )
       {
 #ifdef HAVE_LIBZ
           if ( level >= 0 )
@@ -206,7 +121,7 @@ public:
           return M_comp_level = -1;
       }
 
-    void processMsg( const char * msg,
+void Client::processMsg( const char * msg,
                      const size_t len )
       {
 #ifdef HAVE_LIBZ
@@ -228,7 +143,7 @@ public:
           }
       }
 
-    virtual void parseMsg( const char * msg,
+void Client::parseMsg( const char * msg,
                    const size_t len )
       {
           if ( ! std::strncmp( msg, "(ok compression", 15 ) )
@@ -250,100 +165,11 @@ public:
           std::cout << msg << std::endl;
       }
 
-    int infer_player_loc(Sexpr* expr,float* x,float* y,float* dir) // add verbose flag?
-    {
-      if (strcmp(expr->children[0]->leaf,"see")!=0) return -1;
-      typedef vector<float> socFlag; // x,y,dist,dir
-      vector<socFlag> flags;
-    
-      // loop through objects and find border flags...
-      for (int i=2 ; i<expr->children.size() ; i++) 
-      {
-        printf("see: time=%s object=",expr->children[1]->leaf);
-        expr->children[i]->print_parenthesized();
-        Sexpr* obj=expr->children[i]->children[0];
-        float dist=atof(expr->children[i]->children[1]->leaf);
-        float dir=atof(expr->children[i]->children[2]->leaf);
-    
-        socFlag flag;
-        if (obj->children[0]->leaf[0]=='f')
-        {
-/* beware, if you want to include the cetner flag, you might have to choose the farther solution      
-          if (0 && obj->children.size()==2 && obj->children[1]->leaf[0]=='c')
-          {
-              printf(" * center flag");
-              flag.push_back(0); flag.push_back(0); flag.push_back(dist); flag.push_back(dir);
-              flags.push_back(flag);
-          }
-*/
-          if (obj->children.size()==4)
-            if (strchr("lrtb",obj->children[1]->leaf[0])!=NULL)
-            {
-              float p=999,q=999,z=atof((char*)(obj->children[3]->leaf));
-    	  if (obj->children[1]->leaf[0]=='l') p = -60;
-    	  if (obj->children[1]->leaf[0]=='r') p = 60;
-    	  if (obj->children[1]->leaf[0]=='t') q = 40;
-    	  if (obj->children[1]->leaf[0]=='b') q = -40;
-    	  if (obj->children[2]->leaf[0]=='l') p = -z;
-    	  if (obj->children[2]->leaf[0]=='r') p = z;
-    	  if (obj->children[2]->leaf[0]=='t') q = z;
-    	  if (obj->children[2]->leaf[0]=='b') q = -z;
-              printf(" * flag, coords: %0.1f %0.1f",p,q);
-              flag.push_back(p); flag.push_back(q); flag.push_back(dist); flag.push_back(dir);
-              flags.push_back(flag);
-    	}
-        }
-        printf("\n"); 
-      }
-    
-      if (flags.size()<2) return -1;
-      // find two closest flags (because have least error)
-      int a=0,b=0; // index of flag with min and max angles
-      for (int i=0 ; i<flags.size() ; i++)
-        if (flags[i][2]<flags[a][2]) a = i;
-      if (a==0) b = 1;
-      for (int i=0 ; i<flags.size() ; i++)
-        if (i!=a && flags[i][2]<flags[b][2]) b = i;
-      if (a==b) { printf("warning: can't triangulate position from one flag\n"); return -1; }
-    
-      // transform so a is at (0,0) and b is on X-axis
-      // '1p' is single-prime, which means translated
-      // '2p' is double-prime, which means rotated
-      float xa=flags[a][0],ya=flags[a][1],xb=flags[b][0],yb=flags[b][1],ra=flags[a][2],rb=flags[b][2];
-      float dab=pow(xa-xb,2)+pow(ya-yb,2); if (dab<0) return -1; dab = sqrt(dab);
-      if (ra+rb<dab) { printf("warning: no intersection\n"); return -1; }
-      if (ra>dab+rb || rb>dab+ra) { printf("warning: no intersection - one circle contains the other\n"); return -1; }
-      float xb1p=xb-xa,yb1p=yb-ya; // one prime
-      float theta = atan2(yb1p,xb1p); // ang from a to b; could check for vertical (xb1p=0), but shouldn't occur
-      float xb2p=xb1p*cos(-theta)-yb1p*sin(-theta); // b2p should equal (dab,0)
-      float yb2p=xb1p*sin(-theta)+yb1p*cos(-theta);
-      float x02p=(pow(ra,2)-pow(rb,2)+pow(xb2p,2))/(2.*xb2p);
-      float y02p=pow(ra,2)-pow(x02p,2); if (y02p<0) return -1; y02p = sqrt(y02p); // second solution will be (x02p,-y02p)
-      float x0s1=xa+x02p*cos(theta)-y02p*sin(theta); // unrotate and untranslate to get first solution
-      float y0s1=ya+x02p*sin(theta)+y02p*cos(theta);
-      float x0s2=xa+x02p*cos(theta)+y02p*sin(theta); // unrotate and untranslate to get second solution
-      float y0s2=ya+x02p*sin(theta)-y02p*cos(theta);
-      float d1=(pow(x0s1,2)+pow(y0s1,2)),d2=(pow(x0s2,2)+pow(y0s2,2));
-      float x0=x0s1,y0=y0s1;
-      if (d2<d1) { x0=x0s2; y0=y0s2; } // choose whichever solution is closest to center of field
-      printf("estimate of player location: x=%0.1f y=%0.1f\n",x0,y0);
-      printf("based on flags at (%0.1f,%0.1f) and (%0.1f,%0.1f), dists=(%0.1f,%0.1f), dab=%0.1f, theta=%0.1f\n",xa,ya,xb,yb,ra,rb,dab,theta*180./PI);
-      *x = x0; *y = y0;
-    
-      // compute the direction the player is facing, based on relative angle to closest object
-      float actang=atan2(ya-y0,xa-x0)*180./PI,relang=flags[a][3];
-      *dir = actang+relang; // object to left of center have negative angle
-      //printf("heading: actang=%0.1f, relang=%0.1f\n",actang,relang);
-      printf("heading: actang=%0.1f, relang=%0.1f\n",actang,relang);
-    
-      return 0;
-    }
+void Client::player_initialization() {} // implement in subclass
 
-    // TRI
-    virtual void player_initialization() {}
-    virtual void decide_what_to_do() {}
+void Client::decide_what_to_do() {} // implement in subclass
 
-    void messageLoop()
+void Client::messageLoop()
       {
           fd_set read_fds;
           fd_set read_fds_back;
@@ -441,7 +267,6 @@ public:
               }
           }
       }
-};
 
 //----------------------------------------------------------------------------
 
@@ -455,7 +280,7 @@ public:
 
   PlayerRunsPath( const std::string & server, const int port ) : Client(server,port) {}
 
-  
+ 
   void parseMsg( const char * msg, const size_t len )
   {
     Client::parseMsg(msg,len); // let parent class do its work
@@ -472,6 +297,95 @@ public:
 
    //delete expr;
   }
+
+  int infer_player_loc(Sexpr* expr,float* x,float* y,float* dir) // add verbose flag?
+    {
+      if (strcmp(expr->children[0]->leaf,"see")!=0) return -1;
+      typedef vector<float> socFlag; // x,y,dist,dir
+      vector<socFlag> flags;
+    
+      // loop through objects and find border flags...
+      for (int i=2 ; i<expr->children.size() ; i++) 
+      {
+        printf("see: time=%s object=",expr->children[1]->leaf);
+        expr->children[i]->print_parenthesized();
+        Sexpr* obj=expr->children[i]->children[0];
+        float dist=atof(expr->children[i]->children[1]->leaf);
+        float dir=atof(expr->children[i]->children[2]->leaf);
+    
+        socFlag flag;
+        if (obj->children[0]->leaf[0]=='f')
+        {
+/* beware, if you want to include the cetner flag, you might have to choose the farther solution      
+          if (0 && obj->children.size()==2 && obj->children[1]->leaf[0]=='c')
+          {
+              printf(" * center flag");
+              flag.push_back(0); flag.push_back(0); flag.push_back(dist); flag.push_back(dir);
+              flags.push_back(flag);
+          }
+*/
+          if (obj->children.size()==4)
+            if (strchr("lrtb",obj->children[1]->leaf[0])!=NULL)
+            {
+              float p=999,q=999,z=atof((char*)(obj->children[3]->leaf));
+    	  if (obj->children[1]->leaf[0]=='l') p = -60;
+    	  if (obj->children[1]->leaf[0]=='r') p = 60;
+    	  if (obj->children[1]->leaf[0]=='t') q = 40;
+    	  if (obj->children[1]->leaf[0]=='b') q = -40;
+    	  if (obj->children[2]->leaf[0]=='l') p = -z;
+    	  if (obj->children[2]->leaf[0]=='r') p = z;
+    	  if (obj->children[2]->leaf[0]=='t') q = z;
+    	  if (obj->children[2]->leaf[0]=='b') q = -z;
+              printf(" * flag, coords: %0.1f %0.1f",p,q);
+              flag.push_back(p); flag.push_back(q); flag.push_back(dist); flag.push_back(dir);
+              flags.push_back(flag);
+    	}
+        }
+        printf("\n"); 
+      }
+    
+      if (flags.size()<2) return -1;
+      // find two closest flags (because have least error)
+      int a=0,b=0; // index of flag with min and max angles
+      for (int i=0 ; i<flags.size() ; i++)
+        if (flags[i][2]<flags[a][2]) a = i;
+      if (a==0) b = 1;
+      for (int i=0 ; i<flags.size() ; i++)
+        if (i!=a && flags[i][2]<flags[b][2]) b = i;
+      if (a==b) { printf("warning: can't triangulate position from one flag\n"); return -1; }
+    
+      // transform so a is at (0,0) and b is on X-axis
+      // '1p' is single-prime, which means translated
+      // '2p' is double-prime, which means rotated
+      float xa=flags[a][0],ya=flags[a][1],xb=flags[b][0],yb=flags[b][1],ra=flags[a][2],rb=flags[b][2];
+      float dab=pow(xa-xb,2)+pow(ya-yb,2); if (dab<0) return -1; dab = sqrt(dab);
+      if (ra+rb<dab) { printf("warning: no intersection\n"); return -1; }
+      if (ra>dab+rb || rb>dab+ra) { printf("warning: no intersection - one circle contains the other\n"); return -1; }
+      float xb1p=xb-xa,yb1p=yb-ya; // one prime
+      float theta = atan2(yb1p,xb1p); // ang from a to b; could check for vertical (xb1p=0), but shouldn't occur
+      float xb2p=xb1p*cos(-theta)-yb1p*sin(-theta); // b2p should equal (dab,0)
+      float yb2p=xb1p*sin(-theta)+yb1p*cos(-theta);
+      float x02p=(pow(ra,2)-pow(rb,2)+pow(xb2p,2))/(2.*xb2p);
+      float y02p=pow(ra,2)-pow(x02p,2); if (y02p<0) return -1; y02p = sqrt(y02p); // second solution will be (x02p,-y02p)
+      float x0s1=xa+x02p*cos(theta)-y02p*sin(theta); // unrotate and untranslate to get first solution
+      float y0s1=ya+x02p*sin(theta)+y02p*cos(theta);
+      float x0s2=xa+x02p*cos(theta)+y02p*sin(theta); // unrotate and untranslate to get second solution
+      float y0s2=ya+x02p*sin(theta)-y02p*cos(theta);
+      float d1=(pow(x0s1,2)+pow(y0s1,2)),d2=(pow(x0s2,2)+pow(y0s2,2));
+      float x0=x0s1,y0=y0s1;
+      if (d2<d1) { x0=x0s2; y0=y0s2; } // choose whichever solution is closest to center of field
+      printf("estimate of player location: x=%0.1f y=%0.1f\n",x0,y0);
+      printf("based on flags at (%0.1f,%0.1f) and (%0.1f,%0.1f), dists=(%0.1f,%0.1f), dab=%0.1f, theta=%0.1f\n",xa,ya,xb,yb,ra,rb,dab,theta*180./PI);
+      *x = x0; *y = y0;
+    
+      // compute the direction the player is facing, based on relative angle to closest object
+      float actang=atan2(ya-y0,xa-x0)*180./PI,relang=flags[a][3];
+      *dir = actang+relang; // object to left of center have negative angle
+      //printf("heading: actang=%0.1f, relang=%0.1f\n",actang,relang);
+      printf("heading: actang=%0.1f, relang=%0.1f\n",actang,relang);
+    
+      return 0;
+    }
 
   void player_initialization()
   {
---
+++ rcssserver-15.3.0/src/client.hpp
@@ -0,0 +1,99 @@
+// -*-c++-*-
+
+/***************************************************************************
+                          client.cpp  -  A basic client that connects to
+                          the server
+                             -------------------
+    begin                : 27-DEC-2001
+    copyright            : (C) 2001 by The RoboCup Soccer Server
+                           Maintenance Group.
+    email                : sserver-admin@lists.sourceforge.net
+***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU LGPL as published by the Free Software  *
+ *   Foundation; either version 3 of the License, or (at your option) any  *
+ *   later version.                                                        *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "compress.h"
+
+#include <rcssbase/net/socketstreambuf.hpp>
+#include <rcssbase/net/udpsocket.hpp>
+#include <rcssbase/gzip/gzstream.hpp>
+
+#ifdef HAVE_SSTREAM
+#include <sstream>
+#else
+#include <strstream>
+#endif
+#include <iostream>
+#include <cerrno>
+#include <csignal>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#ifdef __CYGWIN__
+// cygwin is not win32
+#elif defined(_WIN32) || defined(__WIN32__) || defined (WIN32)
+#  define RCSS_WIN
+#  include <winsock2.h>
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h> // select()
+#endif
+#ifdef HAVE_SYS_SELECT_H
+#include <sys/select.h> // select()
+#endif
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h> // select()
+#endif
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h> // select()
+#endif
+
+#include "Sexpr.hpp"
+
+
+class Client {
+//private:
+public:
+    rcss::net::Addr M_dest;
+    rcss::net::UDPSocket M_socket;
+    rcss::net::SocketStreamBuf * M_socket_buf;
+    rcss::gz::gzstreambuf * M_gz_buf;
+    std::ostream * M_transport;
+    int M_comp_level;
+    bool M_clean_cycle;
+
+#ifdef HAVE_LIBZ
+    Decompressor M_decomp;
+#endif
+
+    Client();
+    Client( const Client & );
+    Client & operator=( const Client & );
+
+    Client( const std::string & server,const int port );
+    virtual ~Client();
+    void run();
+    int open();
+    bool bind();
+    void close();
+    int setCompression( int level );
+    void processMsg( const char * msg, const size_t len );
+    virtual void parseMsg( const char * msg, const size_t len );
+    int infer_player_loc(Sexpr* expr,float* x,float* y,float* dir); // TRI
+    virtual void player_initialization(); // TRI
+    virtual void decide_what_to_do(); // TRI
+    void messageLoop();
+};
